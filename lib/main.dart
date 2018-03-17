3209aba1bf2d64dde63ab75bf500390d727f15d9
Unknown <vadstep@gmail.com>
2018-03-17 16:59:51 +0200

    initial

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      title: "Friendlychat",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Friendlychat"),
+        ),
+      ),
+    ),
+  );
+}
